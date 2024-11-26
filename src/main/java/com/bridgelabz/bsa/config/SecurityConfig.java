3ee8e213f347698bcaac785464a8ffe8471c3190
KishanHegade <kishanhegade@gmail.com>
2024-11-26 16:47:04 +0530

    Create bean of PasswordEncoder and AuthenticationManager in SpringConfig class

--- src/main/java/com/bridgelabz/bsa/config/SecurityConfig.java
+++ src/main/java/com/bridgelabz/bsa/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.bridgelabz.bsa.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,4 +33,16 @@ public class SecurityConfig {
                                 .authenticated()
                 ).build();
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+
 }
