dae8d355937081dd2fce8ffb90c10da4372e7acd
KishanHegade <kishanhegade@gmail.com>
2024-11-28 11:13:48 +0530

    Created BookMapper class

--- src/main/java/com/bridgelabz/bsa/dto/BookResponse.java
+++ src/main/java/com/bridgelabz/bsa/dto/BookResponse.java
@@ -1,4 +1,18 @@
 package com.bridgelabz.bsa.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
 public class BookResponse {
+
+    private Integer bookId;
+    private String bookName;
+    private String authorName;
+    private String description;
+    private MultipartFile logo;
+    private Double price;
+    private Integer quantity;
 }
---
+++ src/main/java/com/bridgelabz/bsa/mapper/BookMapper.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.bsa.mapper;
+
+import com.bridgelabz.bsa.dto.BookRequest;
+import com.bridgelabz.bsa.dto.BookResponse;
+import com.bridgelabz.bsa.model.Book;
+
+import java.io.IOException;
+
+public class BookMapper {
+
+    public Book mapToBook(BookRequest bookRequest, Book book) {
+        book.setBookName(bookRequest.getBookName());
+        book.setAuthorName(bookRequest.getAuthorName());
+        book.setDescription(bookRequest.getDescription());
+        try {
+            book.setLogo(bookRequest.getLogo().getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        book.setPrice(bookRequest.getPrice());
+        book.setQuantity(bookRequest.getQuantity());
+        return book;
+    }
+
+    public BookResponse mapToBookResponse(Book book) {
+        BookResponse bookResponse = new BookResponse();
+        bookResponse.setBookId(book.getBookId());
+        bookResponse.setBookName(bookResponse.getBookName());
+        bookResponse.setAuthorName(book.getAuthorName());
+        bookResponse.setDescription(bookResponse.getDescription());
+        bookResponse.setLogo(bookResponse.getLogo());
+        bookResponse.setPrice(bookResponse.getPrice());
+        bookResponse.setQuantity(bookResponse.getQuantity());
+        return bookResponse;
+    }
+}
