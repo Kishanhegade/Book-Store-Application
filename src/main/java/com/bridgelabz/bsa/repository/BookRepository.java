bbd236dd31db6f8fe3fab07edb904a1da030572a
KishanHegade <kishanhegade@gmail.com>
2024-11-28 10:14:31 +0530

    Create BookRepository class

---
+++ src/main/java/com/bridgelabz/bsa/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.bsa.repository;
+
+import com.bridgelabz.bsa.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Integer, Book> {
+}
