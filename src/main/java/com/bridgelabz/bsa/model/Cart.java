874f580ae522f6c8e9de1805141cd0e6da9cfcf6
KishanHegade <kishanhegade@gmail.com>
2024-11-28 15:13:23 +0530

    Create Cart model

---
+++ src/main/java/com/bridgelabz/bsa/model/Cart.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.bsa.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+    private Long totalPrice;
+    private Long quantity;
+}
