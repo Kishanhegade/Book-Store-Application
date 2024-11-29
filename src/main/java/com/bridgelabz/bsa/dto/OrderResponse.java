12902fb68ba09c55ad0e597223a2915717604d06
KishanHegade <kishanhegade@gmail.com>
2024-11-29 12:12:21 +0530

    Create OrderResponse class

---
+++ src/main/java/com/bridgelabz/bsa/dto/OrderResponse.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.bsa.dto;
+
+import java.time.LocalDate;
+
+public class OrderResponse {
+
+    private long orderId;
+    private LocalDate orderDate;
+    private double price;
+    private long quantity;
+    private long UserId;
+    private String address;
+}
