d0bc2427f2b4d9a049e5c3a5fb5f2f15717767a7
KishanHegade <kishanhegade@gmail.com>
2024-11-29 13:02:40 +0530

    Add the fields with validation for OrderRequest dto

--- src/main/java/com/bridgelabz/bsa/dto/OrderRequest.java
+++ src/main/java/com/bridgelabz/bsa/dto/OrderRequest.java
@@ -1,4 +1,28 @@
 package com.bridgelabz.bsa.dto;
 
+import com.bridgelabz.bsa.model.Address;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class OrderRequest {
+
+    @NotNull(message = "Book ID cannot be null")
+    @Positive(message = "Book ID must be a positive number")
+    private long bookId;
+
+    @NotNull(message = "Address cannot be null")
+    @Valid // To validate nested objects
+    private Address address;
+
+    @NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private double price;
+
+
 }
