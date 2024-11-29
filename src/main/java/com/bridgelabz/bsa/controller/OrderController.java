ea7f10ab8c1d844e15c727668ad1945a25ab4713
KishanHegade <kishanhegade@gmail.com>
2024-11-29 13:59:34 +0530

    Modify OrderRequest class to only have Address

--- src/main/java/com/bridgelabz/bsa/controller/OrderController.java
+++ src/main/java/com/bridgelabz/bsa/controller/OrderController.java
@@ -1,12 +1,38 @@
 package com.bridgelabz.bsa.controller;
 
+import com.bridgelabz.bsa.dto.OrderRequest;
+import com.bridgelabz.bsa.dto.OrderResponse;
 import com.bridgelabz.bsa.service.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class OrderController {
 
     private OrderService orderService;
+
+    @PostMapping("/orders/place-order")
+    public ResponseEntity<OrderResponse> placeOrder(OrderRequest orderRequest) {
+
+    }
+
+    public ResponseEntity<OrderResponse> cancelOrder(@PathVariable long orderId) {
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<OrderResponse>> getAllOrders() {
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<OrderResponse>> getAllOrdersForUser(@RequestHeader("Authorization") String authHeader) {
+
+    }
+
+
 }
